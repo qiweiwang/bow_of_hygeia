4f9e76bc00add2fdfd215937bc506a8d549a8b02
qiweiwang <qiweiwang89@gmail.com>
2017-12-19 23:01:04 -0800

    this is the first working version of bow of hygeia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NvD3Module } from 'ng2-nvd3';
+
+
+
 
 
 import { AppComponent } from './app.component';
+import { DrugComponent } from './drug/drug.component';
+import { ModalComponent } from './modal/modal.component';
+
+
+
+import { FdaService } from './fda.service';
+
+
+import 'd3';
+import 'nvd3';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrugComponent,
+    ModalComponent,
+
   ],
   imports: [
-    BrowserModule
+    NgbModule.forRoot(),
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    NvD3Module
   ],
-  providers: [],
+  providers: [FdaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
